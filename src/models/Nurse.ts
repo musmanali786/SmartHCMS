777d39980432642a0bb948639ce7ff27f35861a5
Omar Rehman <omarrehman@gmail.com>
2025-04-20 01:41:46 +0500

    some fixes

--- src/models/Nurse.ts
+++ src/models/Nurse.ts
@@ -1,15 +1,20 @@
-import { Entity, OneToMany, JoinColumn } from "typeorm";
+import { Entity, OneToOne, ManyToOne, OneToMany, JoinColumn, PrimaryColumn } from "typeorm";
 import { User } from "./User";
 import { Department } from "./Department";
 import { Appointment } from "./Appointment";
 
 @Entity()
 export class Nurse {
-  @OneToOne(() => User, (user) => user.nurse, { primary: true })
+  // Define userId as the primary key
+  @PrimaryColumn()
+  userId: number;
+
+  @OneToOne(() => User, (user) => user.nurse, { cascade: true })
   @JoinColumn({ name: "userId" })
   user: User;
 
-  @ManyToOne(() => Department, (department) => department.nurses)
+  @ManyToOne(() => Department, (department) => department.nurses, { nullable: true })
+  @JoinColumn({ name: "departmentId" })
   department: Department;
 
   @OneToMany(() => Appointment, (appointment) => appointment.nurse)
